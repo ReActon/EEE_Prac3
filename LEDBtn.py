2a6c3ce457be3e71685fb648a853face2ede4016
Ryan Acton <actonryan23@gmail.com>
2020-09-25 09:32:44 +0100

    Re-arranged text within main method

--- LEDBtn.py
+++ LEDBtn.py
@@ -3,18 +3,24 @@ import RPi.GPIO as GPIO
 #import sleep to enable LED to blink
 from time import sleep
 
-#configure mode for chosen pin values
-GPIO.setmode(GPIO.BCM)
-
-#a var to keep track of current number
-currentNum = 0
-#a var to monitor the endpoint
-endPoint = 4
-#a note of which GPIO pin belongs to the LED
-LEDPin = 22
 
 def main():
+
+	
+
+	#configure mode for chosen pin values
+	GPIO.setmode(GPIO.BCM)
+
+	#a var to keep track of current number
+	currentNum = 0
+	#a var to monitor the endpoint
+	endPoint = 4
+	#a note of which GPIO pin belongs to the LED
+	LEDPin = 22
+	
 	GPIO.setup(LEDPin, GPIO.OUT)
+
+
 	while currentNum < endPoint:
 		GPIO.output(LEDPin, True)
 		sleep(2)
