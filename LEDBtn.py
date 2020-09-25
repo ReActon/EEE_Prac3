db37326336466055c578f5e947e54e4c224a944f
Ryan Acton <actonryan23@gmail.com>
2020-09-25 09:52:03 +0100

    Added turn off and on within if statements using the buttonPress and ledState booleans

--- LEDBtn.py
+++ LEDBtn.py
@@ -6,32 +6,40 @@ from time import sleep
 
 def main():	
 
-	#configure mode for chosen pin values
-	GPIO.setmode(GPIO.BCM)
+    #configure mode for chosen pin values
+    GPIO.setmode(GPIO.BCM)
 
-	#a var to keep track of current number
-	currentNum = 0
-	#a var to monitor the endpoint
-	endPoint = 4
-	#a note of which GPIO pin belongs to the LED
-	LEDPin = 22
-        #a note of which GPIO pin the button is in
-        buttonPin = 5	
+    #a var to keep track of current number
+    currentNum = 0
+    #a var to monitor the endpoint
+    endPoint = 4
+    #a note of which GPIO pin belongs to the LED
+    LEDPin = 22
+    #a note of which GPIO pin the button is in
+    buttonPin = 5	
 
-	GPIO.setup(LEDPin, GPIO.OUT)
-        GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(LEDPin, GPIO.OUT)
+    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        buttonPress = False
-        ledState = False
-        try:
-	        while currentNum < endPoint:
-	        	GPIO.output(LEDPin, True)
-	            	sleep(2)
-		        GPIO.output(LEDPin, False)
-		        sleep(2)
-		        currentNum += 1
-        finally:
-            GPIO.cleanup()
+    buttonPress = False
+    ledState = False
+    try:	    
+        while currentNum < endPoint:
+            print("Press the button")
+            buttonPress = GPIO.input(buttonPin)
+            if buttonPress == True and ledState == False:
+                GPIO.output(LEDPin, True)
+                ledState = True
+                sleep(3)
+            elif buttonPress == True and ledState == True:
+                GPIO.output(LEDPin, False)
+                ledState = False
+                currentNum += 1
+                sleep(0.5)
+            sleep(0.1)
+	        	
+    finally:
+        GPIO.cleanup()
 
 
 if __name__ == '__main__':
